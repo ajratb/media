8aaa645841e9e2489fdcef82c479a0f294ab1a86
ajratb <ajratb@ya.ru>
2017-09-17 21:43:25 +0700

    delete one of projects

--- SARXOS/webcam-capture-javafx-service/src/main/java/LetsDoIt.java
+++ SARXOS/webcam-capture-javafx-service/src/main/java/LetsDoIt.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ru.waytosky.webcamcapture;
+package main.java;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamResolution;
